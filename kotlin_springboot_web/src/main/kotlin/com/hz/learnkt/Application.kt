7ebc64f022e45c081e103be6b18bc0fb0e097ec7
hezhao <1439293823@qq.com>
2018-06-22 16:31:01 +0800

    提交

--- kotlin_springboot_web/src/main/kotlin/com/hz/learnkt/Application.kt
+++ kotlin_springboot_web/src/main/kotlin/com/hz/learnkt/Application.kt
@@ -9,8 +9,10 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView
 
 // Spring Boot 应用的标识
+// 一般将Application放在最外层包，也就是要包含所有子包。 Spring Boot 会自动加载启动类所在包下及其子包下的所有组件.
+// 也可以手动设置扫描包的路径，可设置多个路径，以下两种方式都可以：
 @SpringBootApplication(scanBasePackages = ["com.hz.learnkt"])
-// @ComponentScan("com.hz.learnkt") // 设置扫描包，以上两种都可以
+// @ComponentScan("com.hz.learnkt")
 class Application {
     /** 在Spring中初始化Jackson */
     @Bean
@@ -22,8 +24,8 @@ class Application {
 /** 运行Spring Boot */
 fun main(args: Array<String>) {
     // 类似Java的启动方式
-     SpringApplication.run(com.hz.learnkt.Application::class.java, *args)
+    // SpringApplication.run(com.hz.learnkt.Application::class.java, *args)
 
     // Kotlin 特有的简便写法
-//    runApplication<Application>(*args)
+    runApplication<Application>(*args)
 }
